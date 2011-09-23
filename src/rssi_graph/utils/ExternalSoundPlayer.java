f2681cd3f5d63720b7fa035ec31c83a167b6f3c4
ph4r05 <ph4r05@ph4.maleus>
2011-09-23 02:50:34 +0200

    external sound player based on ProcessBuilder and playsound

--- src/rssi_graph/utils/ExternalSoundPlayer.java
+++ src/rssi_graph/utils/ExternalSoundPlayer.java
@@ -4,11 +4,49 @@
  */
 package rssi_graph.utils;
 
+import java.io.File;
+
 /**
  * External sound player for java
+ * uses playsound command
  * 
  * @author ph4r05
  */
-public class ExternalSoundPlayer {
+public class ExternalSoundPlayer extends Thread {
+
+    /**
+     * Executable of sound player
+     */
+    public static final String soundPlayer="/usr/bin/playsound";
     
+    private String filename;
+
+    private Position curPosition;
+
+    private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
+
+    enum Position {
+        LEFT, RIGHT, NORMAL
+    };
+
+    public ExternalSoundPlayer(String wavfile) {
+        filename = wavfile;
+        curPosition = Position.NORMAL;
+    }
+
+    @Override
+    public void run() {
+        File soundFile = new File(filename);
+        if (!soundFile.exists()) {
+            System.err.println("Wave file not found: " + filename);
+            return;
+        }
+
+        try {
+            Process p = new ProcessBuilder(ExternalSoundPlayer.soundPlayer, filename).start();
+        } catch(Exception e){
+            e.printStackTrace(System.err);
+        }
+    }
 }
+ 
